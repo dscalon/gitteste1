a267cbda85954bedc50fa6943fbc19f69079a9d2
dscalon <dscalon@live.com>
2020-03-24 22:15:25 -0300

    Adicionada funcionalidade de soma.

--- hello.c
+++ hello.c
@@ -2,7 +2,16 @@
 
 int main ()
 {
-printf("Olá mundo!");
-return 0;
+  int ret;
+  ret = soma(2 ,3);
+  printf("O resultado é %d", ret);
+  return 0;
+
+}
+
+
+int soma (int a, int b)
+{
+  return a + b;
 
 }
